0ac633c80c13df654f5892782e47a4bdbe7d5f61
Quốc Phan <solohacking1256@gmail.com>
2023-09-18 21:16:20 +0700

    IGA first commit

---
+++ IGA/IGA/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DEMSoft.Common;
+using DEMSoft.Drawing;
+using DEMSoft.EngineeringData;
+using DEMSoft.Function;
+using DEMSoft.IGA;
+using DEMSoft.NURBS;
+using DEMSoft.Plot;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace IGA
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            double r = 1;
+            double a = 4;
+            
+            ViewerForm viewer = new ViewerForm(true);
+            List<NURBSSurface> listSurface = new List<NURBSSurface>();
+
+            NURBSSurface[] sur1 = GeometryCreator.CreateSquareWithHoleEightNURBSSurfaces(4,6,r,a);
+
+            //int p0 = 2;
+            //double[,] w = { { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 } };
+            //double[] kv0 = { 0, 0, 0, 0.5, 1, 1, 1 };
+            for (int i = 0; i < sur1.Length; i++)
+            {
+                listSurface.Add(sur1[i]); //patch 0-3
+            }
+
+            foreach (NURBSSurface surface in listSurface)
+            {
+                surface.SetDegreeOnAllDirections(2);
+                surface.isColorfulFace = false;
+                surface.colorControlNet = Color.Blue;
+                surface.colorSurface = Color.Green;
+                surface.colorKnot = Color.Red;
+                surface.colorCurve = Color.Brown;
+                surface.isDrawOriginal = false;
+                surface.colorControlPoint = Color.Brown;
+                surface.Draw(viewer);
+            }
+            viewer.UpdateCamera();
+            viewer.Run();
+        }
+    }
+}
